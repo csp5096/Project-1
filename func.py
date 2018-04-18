7994102d4d620de59920670e56ecd243d85a9be2
root <csp5096@yahoo.com>
2018-04-18 17:26:09 -0500

    First FT specced out in comments, and now used scope sessions as well

--- func.py
+++ func.py
@@ -7,8 +7,41 @@ def browser():
     yield b
     b.quit()
 
-url = 'http://localhost:8000'
+BASE_URL = 'http://localhost:8000'
 
-def test_check_homepage(browser):
-    browser.visit(url)
-    assert browser.is_text_present('Django')
+def url(route):
+    return '{}/{}'.format(BASE_URL, route)
+
+# Edith has heard about a cool new online to-do app.
+
+def test_can_check_homepage(browser):
+    # She goes to check out its homepage
+    browser.visit(url('/'))
+
+    # She notices the page title and header mention to-do lists
+    assert 'To-Do' in browser.title
+
+    # She is invited to enter a to-do item straight away
+
+    # She types "Buy peacock feathers" into a text box
+    # (Edith's hobby is trying fly-fishing lures)
+
+    # When she hits enter, the pages updates, and now the page lists
+    # "1: Buy peacock feathers" as an item in a to-do list
+
+    # There is still a text box inviting her to add another item.
+    # She enters "Use peacock feathers to make a fly"
+    # (Edith is very methodical.)
+
+    # The page updates again, and now shows both items on here list
+
+    # Edith wonders whether the site will remember here lists.
+    # Then she sees that the site had generated a unique URL for her.
+    # The is some explanatory text to that effect.
+
+    # She visits that URL - her to-do list is still there.
+
+    # Satisfied, she goes back to sleep
+
+    # Browser.quit()
+
