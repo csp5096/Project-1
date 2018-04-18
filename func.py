51e5ebe707104d42519d8aef177c3c8c89ba65cd
root <csp5096@yahoo.com>
2018-04-18 15:57:40 -0500

    Redue functional test

--- func.py
+++ func.py
@@ -1,8 +1,14 @@
 import pytest
-from selenium import webdriver
+from splinter import Browser
 
-@pytest.mark.nondestructive
-def test_browser(selenium):
-    #driver = webdriver.Firefox()
-    selenium.get('http://localhost:8000')
-    #assert 'Django' in driver.title
+@pytest.yield_fixture(scope='session')
+def browser():
+    b = Browser()
+    yield b
+    b.quit()
+
+url = 'http://localhost:8000'
+
+def test_check_homepage(browser):
+    browser.visit(url)
+    assert browser.is_text_present('Django')
