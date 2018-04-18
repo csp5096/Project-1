1c7bd71e90614f980584d17238f08ec3eda1ed07
root <csp5096@yahoo.com>
2018-04-18 15:13:44 -0500

    Completed Ch-1

--- func.py
+++ func.py
@@ -1,4 +1,8 @@
-import selenium
+import pytest
+from selenium import webdriver
 
-def test_example(selenium):
+@pytest.mark.nondestructive
+def test_browser(selenium):
+    #driver = webdriver.Firefox()
     selenium.get('http://localhost:8000')
+    #assert 'Django' in driver.title
