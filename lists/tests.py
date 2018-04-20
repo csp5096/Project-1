95b8a21a7ab00a3a3a6a7229c2775475d0e53704
root <csp5096@yahoo.com>
2018-04-19 21:21:34 -0500

    Created class based test for homepage

--- lists/tests.py
+++ lists/tests.py
@@ -4,9 +4,14 @@ from lists.views import home_page
 import pytest
 
 @pytest.mark.django_db
-def test_root_url_resolves_to_home_page_view():
-    found = resolve('/')
-    assert found.func == home_page
+class TestHomePage():
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        assert found.func == home_page
+
+
+
 
 
 
