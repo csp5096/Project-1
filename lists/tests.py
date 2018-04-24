8f888aca10e1d721364f7d9d11c77bb63043a27f
root <csp5096@yahoo.com>
2018-04-24 13:29:34 -0500

    Use RequestFactory for unit testing

--- lists/tests.py
+++ lists/tests.py
@@ -1,23 +1,23 @@
 from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.test import RequestFactory
 from lists.views import home_page
 
 import pytest
 
 @pytest.mark.django_db
-class TestHomePage:
+class TestHomeView:
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        assert found.func == home_page
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
+    def test_uses_home_template(self):
+        request = RequestFactory().get('/')
         response = home_page(request)
-        html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        assert html == expected_html
+        assert response.status_code == 200
+
+    def test_can_save_a_POST_request(self):
+        request = RequestFactory().post('/', data={'item_text': 'A new list item'})
+        response = home_page(request)
+        assert 'A new list item' in response.content.decode()
 
 
 
